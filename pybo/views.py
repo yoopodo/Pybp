9ec797a88b9448d61cb3fe4e4f1681e3141df0d0
유지현 <star_yjh@naver.com>
2022-09-28 14:46:25 +0900

    3-7

--- pybo/views.py
+++ pybo/views.py
@@ -1,28 +1,31 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from .models import Question
-from django.utils import timezone
-from .forms import QuestionForm, Answerform
-from django.http import HttpResponseNotAllowed
 from django.core.paginator import Paginator
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+
+from .forms import QuestionForm, AnswerForm
+from .models import Question
+from django.contrib.auth.decorators import login_required
+
 
 def index(request):
     """
     pybo 목록 출력
     """
-    #입력 인자
-    page = request.GET.get('page', '1') #페이지
+    # 입력 파라미터
+    page = request.GET.get('page', '1')  # 페이지
 
-    #조회
+    # 조회
     question_list = Question.objects.order_by('-create_date')
 
-    #페이징 처리
-    paginator = Paginator(question_list, 10) #페이지당 10개씩 보여주기
+    # 페이징처리
+    paginator = Paginator(question_list, 10)  # 페이지당 10개씩 보여주기
     page_obj = paginator.get_page(page)
 
     context = {'question_list': page_obj}
     return render(request, 'pybo/question_list.html', context)
 
 
+@login_required(login_url='common:login')
 def detail(request, question_id):
     """
     pybo 내용 출력
@@ -31,34 +34,8 @@ def detail(request, question_id):
     context = {'question': question}
     return render(request, 'pybo/question_detail.html', context)
 
-def answer_create(request, question_id):
-    """
-    pybo 답변 등록
-    """
-    question = get_object_or_404(Question, pk=question_id)
-    question.answer_set.create(content=request.POST.get('content'),
-                               create_date=timezone.now())
-    return redirect('pybo:detail', question_id=question.id)
 
-def question_create(request):
-    """
-    pybo 질문 등록
-    """
-    if request.method == 'POST':
-        form = QuestionForm(request.POST)
-        if form.is_valid():
-            question = form.save(commit=False)
-            question.create_date = timezone.now()
-            question.save()
-            return redirect('pybo: index')
-    else:
-        form = QuestionForm()
-    context = {'form': form}
-    return render(request, 'pybo/question_form.html', context)
-
-    form = QuestionForm()
-    return render(request, 'pybo/question_form.html', {'form':form})
-
+@login_required(login_url='common:login')
 def answer_create(request, question_id):
     """
     pybo 답변등록
@@ -68,12 +45,30 @@ def answer_create(request, question_id):
         form = AnswerForm(request.POST)
         if form.is_valid():
             answer = form.save(commit=False)
+            answer.author = request.user  # author 속성에 로그인 계정 저장
             answer.create_date = timezone.now()
             answer.question = question
             answer.save()
             return redirect('pybo:detail', question_id=question.id)
     else:
-        return HttpResponseNotAllowed('Only POST is possible.')
+        form = AnswerForm()
     context = {'question': question, 'form': form}
     return render(request, 'pybo/question_detail.html', context)
 
+
+def question_create(request):
+    """
+    pybo 질문등록
+    """
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.author = request.user  # author 속성에 로그인 계정 저장
+            question.create_date = timezone.now()
+            question.save()
+            return redirect('pybo:index')
+    else:
+        form = QuestionForm()
+    context = {'form': form}
+    return render(request, 'pybo/question_form.html', context)
